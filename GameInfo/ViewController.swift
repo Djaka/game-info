4b96934f29c0f9abd38b21f9f76d284b09730dd3
Djaka Permana <djakapermana95@gmail.com>
2023-06-02 21:51:03 +0700

    Initial Commit

---
+++ GameInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameInfo
+//
+//  Created by Djaka Permana on 02/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
